fd6a5a52f4eb0b8bf8248252befe2a3a6ae955d8
DHarte18 <demiladeh@gmail.com>
2025-04-15 00:08:34 +0100

    WE ARE SO BACK
    Receiver and Car programs are working in full. *NOTE: Solid 5v pwr is needed for wireless connection*. Blue LED indicator not on = ESP_NOW failure

---
+++ Making_it_work/Car_program/Car_program.ino
@@ -0,0 +1,166 @@
+////////////////////////////////CAR PROGRAM/////////////////////////////
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include <Adafruit_VL53L0X.h>
+//#include <iostream>
+using namespace std;
+////////////////////////////////Wireless prep///////////////////////////
+uint8_t destMACaddress[] = {0x20,0x43,0xa8,0x64,0x16,0x20};   //MAC address of PEER ESP32
+uint8_t inSampleRate = 0;                                           //Variable to store desired sample rate from receiver
+uint8_t inReady = 0;
+uint8_t delays = 25;                                          //Variable to store time delay between T-o-F readings
+
+typedef struct struct_message1 {                              //Struct to hold outgoing message
+  int FLdis; int FMdis; int FRdis;
+  int BLdis; int BMdis; int BRdis;
+
+  int FLang; int FRang;
+  int BLang; int BRang;
+} struct_message1;
+struct_message1 outgoingReadings;
+
+typedef struct struct_message2 {                              //Struct to hold incoming ifo
+  int sampleRate;
+  int ready;
+} struct_message2;
+struct_message2 incomingData;
+
+String success;                                               //Variable to store if data was sent successfully
+
+esp_now_peer_info_t peerInfo;                                 //Variabel to store info about PEER
+///////////////////////////////T-o-F Sensor prep/////////////////////////
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+int outFLdis = 0; int outFMdis = 0; int outFRdis = 0;
+int outBLdis = 0; int outBMdis = 0; int outBRdis = 0;
+
+///////////////////////////Rotary Encoder Prep///////////////////////////
+int outFLang = 0; int outFRang = 0;
+int outBLang = 0; int outBRang = 0;
+
+//////////////////Callback function for when data is sent////////////////
+void OnDataSent (const uint8_t* mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status: \t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if(status == 0) {
+    success = "Delivery Success :)";
+  } else {
+    success = "Delivery Failure :(";
+  }
+}
+
+/////////////Callback function for when data is recieved/////////////////
+void OnDataRecv (const uint8_t* mac, const uint8_t* incomingReadings, int len) {
+  memcpy(&incomingData, incomingReadings, sizeof(incomingData));
+  inSampleRate = incomingData.sampleRate;
+  inReady = incomingData.ready;
+  Serial.print(inSampleRate);
+  Serial.println(inReady);
+}
+
+/////////////Function for channel assignment on multiplexer//////////////
+void TCA9548A (uint8_t bus) {
+  Wire.beginTransmission(0x70);                 //Multiplexer address, A0 = GND, A1 = GND, A2 = GND
+  Wire.write(1<<bus);
+  Wire.endTransmission();
+}
+
+////////////////////////////Standard Loops///////////////////////////////  
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  if(esp_now_init() != ESP_OK) {
+    Serial.println("Error initialising ESP_NOW");
+    return;
+  }
+  memcpy(peerInfo.peer_addr, destMACaddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+  if(esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+
+  Wire.begin();
+  while(! Serial) {delay(1);}
+  int i = 2;
+  while(i<8) {
+    TCA9548A(i);
+    if(!lox.begin()) {
+      Serial.println(F("Failed to boot channel ")); Serial.println(i);
+      while(1);
+    } else {;}
+    i++;
+  }
+}
+
+void loop() {
+  TCA9548A(2);//Back Right
+  VL53L0X_RangingMeasurementData_t measure2;
+  lox.rangingTest(&measure2, false);  // pass in 'true' to get debug data printout!
+  if (measure2.RangeStatus != 4) {    // phase failures have incorrect data
+    outBRdis = measure2.RangeMilliMeter; //Update distance measurement
+  } else {
+    outBRdis = 9001;                     //IT'S OVER 9000 (error number)
+  } delay(delays);
+
+  TCA9548A(3);//Front Right
+  VL53L0X_RangingMeasurementData_t measure3;
+  lox.rangingTest(&measure3, false);
+  if(measure3.RangeStatus != 4) {
+    outFRdis = measure3.RangeMilliMeter;
+  } else {
+    outFRdis = 9001;
+  } delay(delays);
+
+  TCA9548A(4);//Front Middle
+  VL53L0X_RangingMeasurementData_t measure4;
+  lox.rangingTest(&measure4, false);
+  if(measure4.RangeStatus != 4) {
+    outFMdis = measure4.RangeMilliMeter;
+  } else {
+    outFMdis = 9001;
+  } delay(delays);
+
+  TCA9548A(5);//Front Left
+  VL53L0X_RangingMeasurementData_t measure5;
+  lox.rangingTest(&measure5, false);
+  if(measure5.RangeStatus != 4) {
+    outFLdis = measure5.RangeMilliMeter;
+  } else {
+    outFLdis = 9001;
+  } delay(delays);
+  
+  TCA9548A(6);//Back Middle
+  VL53L0X_RangingMeasurementData_t measure6;
+  lox.rangingTest(&measure6, false);
+  if(measure6.RangeStatus != 4) {
+    outBMdis = measure6.RangeMilliMeter;
+  } else {
+    outBMdis = 9001;
+  } delay(delays);
+
+  TCA9548A(7);//Back Left
+  VL53L0X_RangingMeasurementData_t measure7;
+  lox.rangingTest(&measure7, false);
+  if(measure7.RangeStatus != 4) {
+    outBLdis = measure7.RangeMilliMeter;
+  } else {
+    outBLdis = 9001;
+  } delay(delays);
+  //Copy sensor readings to struct message to be sent
+  outgoingReadings.FLdis = outFLdis; outgoingReadings.FMdis = outFMdis; outgoingReadings.FRdis = outFRdis;
+  outgoingReadings.BLdis = outBLdis; outgoingReadings.BMdis = outBMdis; outgoingReadings.BRdis = outBRdis;
+
+  outgoingReadings.FLang = outFLang; outgoingReadings.FRang = outFRang;
+  outgoingReadings.BLang = outBLang; outgoingReadings.BRang = outBRang;
+  //Send struct message
+  esp_err_t result = esp_now_send(destMACaddress, (uint8_t*) &outgoingReadings, sizeof(outgoingReadings));
+  if(result == ESP_OK) {
+    Serial.println("Sent Successfully");
+  } else {
+    Serial.println("Error Sending");
+  }
+  delay(inSampleRate);
+}
--- Making_it_work/Reciever_program/Reciever_program.ino
+++ Making_it_work/Reciever_program/Reciever_program.ino
@@ -11,13 +11,27 @@ using namespace std;
 uint8_t destMACaddress[] = {0xf0,0x24,0xf9,0x7a,0x39,0x14};   //MAC address of peer ESP32 NOT the ESP32 this code is running on
 uint8_t incomingToFArr[1][6];                                 //Array to store recieved T-o-F data
 uint8_t incomingRotEncArr[1][4];                              //Array to store recieved rotary encoder data
-uint8_t sampleRate = 0;                                       //Variable for sample rate to be sent in milliseconds
+uint8_t outSampleRate = 1000;                                           //Variable for sample rate to be sent in milliseconds
+uint8_t outReady = 0;
+int FLdis = 0; int FMdis = 0; int FRdis = 0;
+int BLdis = 0; int BMdis = 0; int BRdis = 0;
+int FLang = 0; int FRang = 0;
+int BLang = 0; int BRang = 0;
 
-typedef struct struct_message {                             //Struct to hold incoming readings
-  uint8_t ToFArr[1][6];
-  uint8_t RotEncArr[1][4];
-} struct_message;
-struct_message incomingReadings;
+typedef struct struct_message1 {                               //Struct to hold incoming readings
+  int FLdis; int FMdis; int FRdis;
+  int BLdis; int BMdis; int BRdis;
+
+  int FLang; int FRang;
+  int BLang; int BRang;
+} struct_message1;
+struct_message1 incomingReadings;
+
+typedef struct struct_message2 {
+  int sampleRate;
+  int ready;
+} struct_message2;
+struct_message2 outgoingData;
 
 String success;                                               //Variable to store if sending data was successful
 
@@ -37,20 +51,14 @@ void OnDataSent (const uint8_t* mac_addr, esp_now_send_status_t status) {
 //////////////////Callback function for when data is recieved//////////////////
 void OnDataRecv (const uint8_t* mac, const uint8_t* incomingData, int len) {
   memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
-  //incomingToFArr = incomingReadings.ToFArr;
-  //incomingRotEncArr = incomingReadings.RotEncArr;
-  int i, j;
-  for (i=0; i<1; i++) {
-    for (j=0; j<6; j++) {
-      cout<<"\t"<<incomingReadings.ToFArr[i][j];
-    } cout<<endl;
-  }
-  int g, h;
-  for (g=0; g<1; g++) {
-    for (h=0; h<1; h++) {
-      cout<<"\t"<<incomingReadings.RotEncArr[g][h];
-    } cout<<endl;
-  }
+  FLdis = incomingReadings.FLdis; FMdis = incomingReadings.FMdis; FRdis = incomingReadings.FRdis;
+  BLdis = incomingReadings.BLdis; BMdis = incomingReadings.BMdis; BRdis = incomingReadings.BRdis;
+
+  FLang = incomingReadings.FLang; FRang = incomingReadings.FRang;
+  BLang = incomingReadings.BLang; BRang = incomingReadings.BRang;
+  cout<<"\n\r"<<FLdis<<" "<<FMdis<<" "<<FRdis;
+  cout<<"\n\r"<<BLdis<<" "<<BMdis<<" "<<BRdis;
+  cout<<endl;
 }
 
 /////////////////////////////////Normal Loops//////////////////////////////////
@@ -78,12 +86,13 @@ void setup() {
 }
 
 void loop() {
-  sampleRate = 100;
+  outgoingData.sampleRate = outSampleRate;
+  outgoingData.ready = outReady;
   //Send message w/ ESP_NOW
-  esp_err_t result = esp_now_send(destMACaddress, (uint8_t*) &sampleRate, sizeof(sampleRate));
+  esp_err_t result = esp_now_send(destMACaddress, (uint8_t*) &outgoingData, sizeof(outgoingData));
   if (result == ESP_OK) {
     Serial.println("Sent successfully");
   } else {
     Serial.println("Error Sending");
-  } delay(10000);
+  } delay(outSampleRate);
 }
